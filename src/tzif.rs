f73ba72b9963ec8cac43cc236a36b86d57cd2a0d
Arie Bovenberg <a.c.bovenberg@gmail.com>
2025-03-01 10:45:37 +0100

    tzif: parse trans_utc, trans_idx

--- src/tzif.rs
+++ src/tzif.rs
@@ -15,14 +15,15 @@ pub(crate) struct Header {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct TZif {
     header: Header,
+    trans_utc: Vec<i64>,
+    trans_idx: Vec<u8>,
 }
 
 #[allow(dead_code)]
 pub(crate) fn parse(mut s: impl Read + Seek) -> ParseResult<TZif> {
     let header = parse_header(&mut s)?;
     debug_assert_eq!(header.version, 2);
-    parse_data(header, &mut s)?;
-    Ok(TZif { header })
+    parse_content(header, &mut s)
 }
 
 // Check if the magic bytes are present
@@ -62,18 +63,48 @@ fn parse_header(mut s: impl Read + Seek) -> ParseResult<Header> {
     })
 }
 
-fn parse_data(header: Header, mut s: impl Read + Seek) -> ParseResult<()> {
+fn parse_trans_utc(header: Header, mut s: impl Read + Seek) -> ParseResult<Vec<i64>> {
+    debug_assert_eq!(header.version, 2);
+    debug_assert!(header.timecnt > 0);
+    // OPTIMIZE: how about using smallvec?
+    let mut result = Vec::with_capacity(header.timecnt as usize);
+    let mut buffer = [0u8; std::mem::size_of::<i64>()];
+    for _ in 0..header.timecnt {
+        s.read_exact(&mut buffer)?;
+        result.push(i64::from_be_bytes(buffer));
+    }
+    Ok(result)
+}
+
+fn parse_trans_idx(header: Header, mut s: impl Read + Seek) -> ParseResult<Vec<u8>> {
+    debug_assert_eq!(header.version, 2);
+    debug_assert!(header.timecnt > 0);
+    // OPTIMIZE: how about using smallvec?
+    let mut result = Vec::with_capacity(header.timecnt as usize);
+    let mut buffer = [0u8; std::mem::size_of::<u8>()];
+    for _ in 0..header.timecnt {
+        s.read_exact(&mut buffer)?;
+        result.push(u8::from_be_bytes(buffer));
+    }
+    Ok(result)
+}
+
+fn parse_content(first_header: Header, mut s: impl Read + Seek) -> ParseResult<TZif> {
     s.seek(SeekFrom::Current(
-        (header.timecnt * 5
-            + header.typecnt * 6
-            + header.charcnt
-            + header.leapcnt * 8
-            + header.isstdcnt
-            + header.isutcnt) as i64,
+        (first_header.timecnt * 5
+            + first_header.typecnt * 6
+            + first_header.charcnt
+            + first_header.leapcnt * 8
+            + first_header.isstdcnt
+            + first_header.isutcnt) as i64,
     ))?;
+    // This "second" header is not the same as the first one
     let header = parse_header(&mut s)?;
-    println!("{:?}", header);
-    Ok(())
+    Ok(TZif {
+        header,
+        trans_utc: parse_trans_utc(header, &mut s)?,
+        trans_idx: parse_trans_idx(header, &mut s)?,
+    })
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
@@ -180,19 +211,222 @@ mod tests {
 
     #[test]
     fn test_example() {
+        let tzif = parse(&mut Cursor::new(TZFILE)).unwrap();
         assert_eq!(
-            parse(&mut Cursor::new(TZFILE)).unwrap(),
-            TZif {
-                header: Header {
-                    version: 2,
-                    isutcnt: 11,
-                    isstdcnt: 11,
-                    leapcnt: 0,
-                    timecnt: 184,
-                    typecnt: 11,
-                    charcnt: 22
-                }
+            tzif.header,
+            Header {
+                version: 2,
+                isutcnt: 12,
+                isstdcnt: 12,
+                leapcnt: 0,
+                timecnt: 185,
+                typecnt: 12,
+                charcnt: 26
             }
         );
+        assert_eq!(
+            tzif.trans_utc,
+            &[
+                -2840141850,
+                -2450995200,
+                -1740355200,
+                -1693702800,
+                -1680483600,
+                -1663455600,
+                -1650150000,
+                -1632006000,
+                -1618700400,
+                -1613826000,
+                -1604278800,
+                -1585530000,
+                -1574038800,
+                -1552266000,
+                -1539997200,
+                -1520557200,
+                -1507510800,
+                -1490576400,
+                -1473642000,
+                -1459126800,
+                -1444006800,
+                -1427677200,
+                -1411952400,
+                -1396227600,
+                -1379293200,
+                -1364778000,
+                -1348448400,
+                -1333328400,
+                -1316394000,
+                -1301263200,
+                -1284328800,
+                -1269813600,
+                -1253484000,
+                -1238364000,
+                -1221429600,
+                -1206914400,
+                -1191189600,
+                -1175464800,
+                -1160344800,
+                -1143410400,
+                -1127685600,
+                -1111960800,
+                -1096840800,
+                -1080511200,
+                -1063576800,
+                -1049061600,
+                -1033336800,
+                -1017612000,
+                -1002492000,
+                -986162400,
+                -969228000,
+                -950479200,
+                -942012000,
+                -934668000,
+                -857257200,
+                -844556400,
+                -828226800,
+                -812502000,
+                -798073200,
+                -781052400,
+                -766623600,
+                -745455600,
+                -733273200,
+                228877200,
+                243997200,
+                260326800,
+                276051600,
+                291776400,
+                307501200,
+                323830800,
+                338950800,
+                354675600,
+                370400400,
+                386125200,
+                401850000,
+                417574800,
+                433299600,
+                449024400,
+                465354000,
+                481078800,
+                496803600,
+                512528400,
+                528253200,
+                543978000,
+                559702800,
+                575427600,
+                591152400,
+                606877200,
+                622602000,
+                638326800,
+                654656400,
+                670381200,
+                686106000,
+                701830800,
+                717555600,
+                733280400,
+                749005200,
+                764730000,
+                780454800,
+                796179600,
+                811904400,
+                828234000,
+                846378000,
+                859683600,
+                877827600,
+                891133200,
+                909277200,
+                922582800,
+                941331600,
+                954032400,
+                972781200,
+                985482000,
+                1004230800,
+                1017536400,
+                1035680400,
+                1048986000,
+                1067130000,
+                1080435600,
+                1099184400,
+                1111885200,
+                1130634000,
+                1143334800,
+                1162083600,
+                1174784400,
+                1193533200,
+                1206838800,
+                1224982800,
+                1238288400,
+                1256432400,
+                1269738000,
+                1288486800,
+                1301187600,
+                1319936400,
+                1332637200,
+                1351386000,
+                1364691600,
+                1382835600,
+                1396141200,
+                1414285200,
+                1427590800,
+                1445734800,
+                1459040400,
+                1477789200,
+                1490490000,
+                1509238800,
+                1521939600,
+                1540688400,
+                1553994000,
+                1572138000,
+                1585443600,
+                1603587600,
+                1616893200,
+                1635642000,
+                1648342800,
+                1667091600,
+                1679792400,
+                1698541200,
+                1711846800,
+                1729990800,
+                1743296400,
+                1761440400,
+                1774746000,
+                1792890000,
+                1806195600,
+                1824944400,
+                1837645200,
+                1856394000,
+                1869094800,
+                1887843600,
+                1901149200,
+                1919293200,
+                1932598800,
+                1950742800,
+                1964048400,
+                1982797200,
+                1995498000,
+                2014246800,
+                2026947600,
+                2045696400,
+                2058397200,
+                2077146000,
+                2090451600,
+                2108595600,
+                2121901200,
+                2140045200
+            ]
+        );
+        assert_eq!(
+            tzif.trans_idx,
+            &[
+                1, 2, 3, 6, 3, 4, 5, 4, 5, 9, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8,
+                7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 7, 4, 5, 4,
+                5, 4, 5, 4, 5, 4, 5, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10,
+                11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11,
+                10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10,
+                11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11,
+                10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10,
+                11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11,
+                10, 11
+            ]
+        )
     }
 }
