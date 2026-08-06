b03137c7798bda15d738d9e0dd3ed91763523022
Arie Bovenberg <a.c.bovenberg@gmail.com>
2026-08-06 21:52:57 +0200

    Mirror pattern grammar in Rust

--- src/classes/offset_datetime.rs
+++ src/classes/offset_datetime.rs
@@ -976,8 +976,12 @@ fn format(cls: PyClass<OffsetDateTime>, slf: OffsetDateTime, pattern_obj: PyObj)
         .ok_or_type_err("format() argument must be str")?;
     let pattern_str = pattern_pystr.as_utf8()?;
     let pattern = pattern::CompiledPattern::compile(pattern_str).into_value_err()?;
-    pattern.validate(pattern::CategorySet::DATE_TIME_OFFSET, "OffsetDateTime")?;
-    pattern.warn_if_ambiguous_12h(*cls.state().warn_whenever)?;
+    pattern.validate(
+        pattern::CategorySet::DATE_TIME_OFFSET,
+        "OffsetDateTime",
+        *cls.state().warn_whenever,
+        *cls.state().warn_deprecation,
+    )?;
     pattern.format(&slf.to_plain().pattern_values().with_offset(slf.offset))
 }
 
@@ -1003,7 +1007,12 @@ fn parse(cls: PyClass<OffsetDateTime>, args: &[PyObj], kwargs: &mut IterKwargs) 
     let fmt_bytes = fmt_pystr.as_utf8()?;
 
     let pattern = pattern::CompiledPattern::compile(fmt_bytes).into_value_err()?;
-    pattern.validate(pattern::CategorySet::DATE_TIME_OFFSET, "OffsetDateTime")?;
+    pattern.validate(
+        pattern::CategorySet::DATE_TIME_OFFSET,
+        "OffsetDateTime",
+        *cls.state().warn_whenever,
+        *cls.state().warn_deprecation,
+    )?;
     let parsed = pattern.parse(s).into_value_err()?;
     let offset = parsed
         .offset_secs
