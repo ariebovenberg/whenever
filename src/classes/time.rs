a14144ffed8cc07504d9a67950579fe10b19e629
Arie Bovenberg <a.c.bovenberg@gmail.com>
2026-08-06 21:52:57 +0200

    Mirror pattern grammar in Rust

--- src/classes/time.rs
+++ src/classes/time.rs
@@ -299,8 +299,12 @@ fn format(cls: PyClass<Time>, slf: Time, pattern_obj: PyObj) -> PyReturn {
         .ok_or_type_err("format() argument must be str")?;
     let pattern_str = pattern_pystr.as_utf8()?;
     let pattern = pattern::CompiledPattern::compile(pattern_str).into_value_err()?;
-    pattern.validate(pattern::CategorySet::TIME, "Time")?;
-    pattern.warn_if_ambiguous_12h(*cls.state().warn_whenever)?;
+    pattern.validate(
+        pattern::CategorySet::TIME,
+        "Time",
+        *cls.state().warn_whenever,
+        *cls.state().warn_deprecation,
+    )?;
     pattern.format(&slf.pattern_values())
 }
 
@@ -326,7 +330,12 @@ fn parse(cls: PyClass<Time>, args: &[PyObj], kwargs: &mut IterKwargs) -> PyRetur
     let fmt_bytes = fmt_pystr.as_utf8()?;
 
     let pattern = pattern::CompiledPattern::compile(fmt_bytes).into_value_err()?;
-    pattern.validate(pattern::CategorySet::TIME, "Time")?;
+    pattern.validate(
+        pattern::CategorySet::TIME,
+        "Time",
+        *cls.state().warn_whenever,
+        *cls.state().warn_deprecation,
+    )?;
     pattern.parse(s).into_value_err()?.time()?.to_obj(cls)
 }
 
